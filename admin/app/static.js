9524ff49334b062bc73f571481d931ac4372cd7e
Dennis Pettersson <mail@dennispettersson.se>
2016-11-22 10:56:17 +0100

    corrected paths

--- admin/app/static.js
+++ admin/app/static.js
@@ -12,24 +12,34 @@ var debug = require('debug')('keystone:admin:app:static');
 var express = require('express');
 var packages = require('../packages');
 var router = express.Router();
+var main = require('path').dirname(require.main.filename);
+var fs = require('fs');
 
-router.use('/styles', require('less-middleware')(__dirname + '../../../public/styles'));
-router.use(express.static(__dirname + '../../../public'));
+var root;
 
-function doBrowserify(path) {
-	router.use('/js', browserify(__dirname + path, {
+try {
+	fs.accessSync(`${main}/admin/src`, fs.F_OK);
+	root = `${main}`;
+} catch (_error) {
+	fs.accessSync(`${__dirname}/../../admin/src`, fs.F_OK);
+	root = require('path').normalize(`${__dirname}/../..`);
+}
+
+router.use('/styles', require('less-middleware')(`${root}/public/styles`));
+router.use(express.static(`${root}/public`));
+
+function doBrowserify (path) {
+	router.use('/js', browserify(`${root}/${path}`, {
 		external: packages,
 		transform: [babelify]
 	}));
 }
 
 try {
-	doBrowserify('../../../admin/src/views')
-} catch (e) {
-	console.log('browserify(...) error', e)
-	console.log('Attempting fail-safe override...')
-
-	doBrowserify('/../../admin/src/views')
+	doBrowserify('src/views');
+} catch (_e) {
+	console.log('Attempting fail-safe override...');
+	doBrowserify('admin/src/views');
 }
 
 module.exports = router;
